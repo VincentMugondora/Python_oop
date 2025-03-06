e432da2b154011929ac61f803856eaa57770ea36
VincentMugondora <vinmugondora@gmail.com>
2025-03-06 15:50:56 +0200

    finished basic inheritance

--- inheritance.py
+++ inheritance.py
@@ -2,3 +2,50 @@
 # Parent class = The class being inherited from
 # Child class = The class that inherits from another class
 # Helps with code reusability and extensibility
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+        self.is_alive = True
+
+    def eat(self):
+        print(f"{self.name} is eating") 
+
+    def sleep(self):
+        print(f"{self.name} is sleeping")
+
+class Dog(Animal):
+    def speak(self):
+        print(f"{self.name} is barking")
+
+class Cat(Animal):
+    def speak(self):
+        print(f"{self.name} is meowing")
+
+class Mouse(Animal):
+    def speak(self):
+        print(f"{self.name} is squeaking")
+
+dog = Dog("Buddy")
+cat = Cat("Whiskers")
+mouse = Mouse("Squeaky")
+
+print(dog.name)
+print(cat.name)
+print(mouse.name)
+
+print(dog.is_alive)
+print(cat.is_alive)
+print(mouse.is_alive)
+
+dog.eat()
+cat.eat()
+mouse.eat()
+    
+dog.sleep()
+cat.sleep()
+mouse.speak()
+
+dog.speak()
+cat.speak()
+mouse.speak()
