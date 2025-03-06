5508adee2396e029288c8f01431c1649cc876463
VincentMugondora <vinmugondora@gmail.com>
2025-03-06 13:26:22 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+# object = a bundle of related attributes (variables) and mthods (functions)
+#        => Ex. phone, cup, book
+#        => You need a class to create many objects
+
+
+# class = a blueprint used to design the structure of an object
+
+
+class Car:
+    def __init__(self):
+        
